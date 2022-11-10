69a94aa9a4ffbfb9d81de085e9a56743b6c45b4b
Upoma218 <upoma.cdsr@gmail.com>
2022-11-10 07:10:28 +0600

    service section start

--- src/Pages/Services/Services/Services.js
+++ src/Pages/Services/Services/Services.js
@@ -14,7 +14,8 @@ const Services = () => {
     return (
         <div className=' bg-base-100 shadow-xl max-w-screen-xl mx-auto my-24 '>
             <h1 className='text-3xl font-bold text-center'>Services</h1>
-            <p className='px-36 py-6 text-center'>You can get all type of service has given bellow! <br /></p>
+            <p className='px-36 py-6 text-center'>You can get all type of service has given bellow! <br />If you want hire me for party dishes, you have to confirm booking minimum 15 days before your programme!<br /> You can contact me for details.
+            </p>
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 py-12'>
                 {
                     services.map(service => <Services key={service._id} service={service}></Services>)
